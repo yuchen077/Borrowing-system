78bdd6e064300e9423ca988b718b89767b9c561b
yuchen077 <eric850810@gmail.com>
2024-02-25 17:51:02 +0800

    初始化 Spring Boot Project

---
+++ Borrowing-system/src/main/java/com/yuchen/borrowingsystem/BorrowingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yuchen.borrowingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorrowingSystemApplication.class, args);
+	}
+
+}
